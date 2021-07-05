a5a7360c39bd91054dd07c6281e1544f0f874682
Taufan <taufanderazor@gmail.com>
2021-07-05 20:45:00 +0700

    Add project files.

---
+++ YPS/repository/Repository.vb
@@ -0,0 +1,17 @@
+﻿Public Class Repository
+    Private Shared objRepository As Repository
+    Private Sub New()
+        koneksiDB()
+    End Sub
+
+    Public Shared Function getInstance() As Repository
+        If (objRepository Is Nothing) Then
+            objRepository = New Repository()
+        End If
+        Return objRepository
+    End Function
+
+
+
+
+End Class
